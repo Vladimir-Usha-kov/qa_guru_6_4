798d9488a9ce33723fbafbc21a54c58a26a4f029
Vladimir <ushakov.vladimir98@mail.ru>
2023-06-28 14:04:28 +0400

    Reformat Code

--- Qa_guru_4.py
+++ Qa_guru_4.py
@@ -6,9 +6,8 @@ def test_greeting():
     name = "Анна"
     age = 25
     output = f"Привет, {name}! Тебе {age} лет."
-    print(output)
     assert output == "Привет, Анна! Тебе 25 лет."
-
+    print(output)
 
 
 def test_rectangle():
@@ -25,7 +24,6 @@ def test_circle():
     r = 23
     area = (r ** 2) * math.pi
     assert area == 1661.9025137490005
-
     length = 2 * math.pi * r
     assert length == 144.51326206513048
     print(length, area)
@@ -51,8 +49,7 @@ def test_unique_elements():
 def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
-    # TODO создайте словарь
     d = dict(zip(first, second))
-    print(d.values())
     assert isinstance(d, dict)
     assert len(d) == 5
+    print(d.values())
